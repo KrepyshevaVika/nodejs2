07dd1c9b5cbe24f00fda11572df806f075137847
KrepyshevaVika <27vita03@mail.ru>
2018-10-23 22:55:40 +0300

    t

--- server.js
+++ server.js
@@ -2,27 +2,26 @@ var express = require("express");
 var app = require("express")();
 var cors = require('cors')
 var bodyParser = require('body-parser');
-var Pusher = require('pusher');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(bodyParser.json());
 app.use(cors());
 //app.use(express.static('../react/build'));
-const pusher = new Pusher({
-    key: 'APP_KEY',
-});
 
+io.on('connection', function(client){
+
+    client.on('subscribeToSlider', (targetValue) => {
+        
+
+        client.broadcast.emit( 'slider', { targetValue, senderId: client.id } );
+    });
+});
+  
 require('./route/node.route.js')(app);
-
-app.post('/slider', (req, res) => {
-    console.log(req.body);
-    const payload = req.body;
-    pusher.trigger('slider', 'targetValue', payload);
-    console.log(targetValue);
-    res.send(payload)
-});
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log(`Server is running.. on Port: ${PORT}`);
 });
